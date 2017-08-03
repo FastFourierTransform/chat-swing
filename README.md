95cad6a7e2d2449053e98a27df95a11f3d9837b1
tiago alexandre melo almeida <tiagomeloalmeida@ua.pt>
2017-08-03 16:38:19 +0100

    Start of main logic and protocol definition

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # chat-swing
+Real time chat working over the network using to demonstrate the [client-server-library](https://github.com/FastFourierTransform/client-server-library) functionality.
+
+## Features
+Simple server side without GUI, one server handle one chat group.
+Client side with GUI in java swing, different clients comunicate with each other that are linked to one server
+In high level all communications are send in json format
+
+### Json protocol
+```
+{command:(command),src:(sender),[msg:message]}
+Command:
+ * enter
+ * leave
+ * message
+``` 
--- src/main/java/com/pt/Main.java
+++ src/main/java/com/pt/Main.java
@@ -5,6 +5,16 @@
  */
 package com.pt;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+import com.pt.exceptions.ServerAlreadyUsePort;
+import com.pt.implementation.server.Server;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.ProtocolException;
+import java.util.Scanner;
+
 /**
  *
  * @author Tiago Alexandre Melo Almeida
@@ -14,8 +24,49 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws ServerAlreadyUsePort {
+
+        switch (args[0]) {
+            case "-s"://-s (portnumber)
+                launchInServerMode(Integer.parseInt(args[1]));
+                break;
+            case "-c"://-c (host) (portnumber)
+                launchInClientMode(args[1],Integer.parseInt(args[2]));
+                break;
+            default:
+                System.out.println("Invalid argument: " + args[0]);
+                break;
+
+        }
     }
-    
+
+    private static void launchInServerMode(int portNumber) throws ServerAlreadyUsePort {
+        System.out.println("Starting Server on port " + portNumber);
+        Server server = new Server(50, 1);
+        
+        //start listing client connections
+        server.startListningConnections(portNumber, (in, out) -> {
+            //logic to handle each connection
+            //messages are in json format
+            JsonObject jObject = new JsonParser().parse(new InputStreamReader(in)).getAsJsonObject();
+            String command = jObject.get("command").getAsString();
+            
+            switch (command){
+                case "enter":
+                    //TODO client-server-library precisa de ter algo para aceder a todos os clientes conetados.. standby
+                    break;
+                case "leave":
+                    break;
+                case "message":
+                    break;
+                default:
+                    throw new ProtocolException("Command invalid " + command);
+            }
+        });
+    }
+
+    private static void launchInClientMode(String host, int portNumber) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
